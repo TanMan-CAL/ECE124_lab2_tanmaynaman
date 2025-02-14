b89d9207993c501536bab8ad5b3085d0bc500de8
Tanmay Shah <70305211+TanMan-CAL@users.noreply.github.com>
2025-02-13 23:40:46 -0500

    Update special_com.vhd

--- special_com.vhd
+++ special_com.vhd
@@ -16,6 +16,6 @@ architecture mux_logic of special_com is
 begin
 	-- 2 -1 multiplexer logic based on the push button selector
 	with logic_select2 select
-		logic_out2 <= logic_num0 when '0',  	-- logic_select2 is '0' then output logic_out2 gets the value of logic_num0	
-			      logic_num1 when '1';	-- logic_select2 is '1' then output logic_out2 gets the value of logic_num1
+		logic_out2 <= logic_num0 when '0', 	
+			      logic_num1 when '1';	
 end mux_logic;
